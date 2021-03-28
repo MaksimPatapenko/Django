5297348becf3b4f77732db3d39ca7d595a114620
Maksim <xxxralphlaurenxxx@gmail.com>
2021-03-28 21:15:27 +0300

    8.2. Общая логика. Ajax на стороне темплейта. Сохраненние данных из Ajax на стороне сервера. Добавление отслеживаемого ивента + считывание данных о товаре. Добавлена модель для корзины.

--- products/views.py
+++ products/views.py
@@ -4,4 +4,9 @@ from products.models import *
 
 def product(request, product_id):
     product = Product.objects.get(id=product_id)
+
+    session_key = request.session.session_key
+    # Для незарегистрированных пользователей создаём ключ вручную
+    if not session_key:
+        request.session.cycle_key()
     return render(request, 'products/product.html', locals())
--- static/static_dev/js/scripts.js
+++ static/static_dev/js/scripts.js
@@ -1,23 +1,40 @@
 $ (document).ready(function () {
    let form = $('#form_buying_product');
-   console.log(form);
    form.on('submit', function (e) {
        e.preventDefault();
-       console.log('123');
        let nmb = $('#number').val();
-       console.log(nmb);
        let submit_btn = $('#submit_btn');
        let product_id = submit_btn.data("product_id");
        let name = submit_btn.data("name");
        let price = submit_btn.data("price");
-       console.log(submit_btn);
-       console.log(product_id);
-       console.log(name);
-       console.log(price);
 
        $('.basket-items ul').append('<li>'+name+', ' + nmb + 'шт. ' + 'по ' + price + 'грн  ' +
             '<a class="delete-item" href="">x</a>'+
             '</li>');
+
+       let data = {};
+       data.product_id = product_id;
+       data.nmb = nmb;
+       let csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
+       data["csrfmiddlewaretoken"] = csrf_token;
+       let url = form.attr("action");
+       console.log(data);
+       $.ajax({
+           url: url,
+           type: 'POST',
+           data: data,
+           cache: true,
+           success: function (data) {
+               console.log("OK");
+               console.log(data.products_total_nmb);
+               if (data.products_total_nmb) {
+                   $('#basket_total_nmb').text('('+data.products_total_nmb+')')
+               }
+           },
+           error: function () {
+               console.log("Error");
+           },
+       });
 
    });
 
